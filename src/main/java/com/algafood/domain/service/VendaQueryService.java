79f327436f3e1d8dd39156980ca78e6e928c40d0
bruno <enzo_jpn@hotmail.com>
2021-10-12 00:17:12 -0300

    criando controller de estatisticas

---
+++ src/main/java/com/algafood/domain/service/VendaQueryService.java
@@ -0,0 +1,10 @@
+package com.algafood.domain.service;
+
+import java.util.List;
+
+import com.algafood.domain.filter.VendaDiariaFilter;
+import com.algafood.domain.model.dto.VendaDiaria;
+
+public interface VendaQueryService {
+	List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter filter);
+}
---
+++ src/main/java/com/algafood/infrastructure/service/VendaQueryServiceImpl.java
@@ -0,0 +1,44 @@
+package com.algafood.infrastructure.service;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CompoundSelection;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.Root;
+
+import org.springframework.stereotype.Repository;
+
+import com.algafood.domain.filter.VendaDiariaFilter;
+import com.algafood.domain.model.Pedido;
+import com.algafood.domain.model.dto.VendaDiaria;
+import com.algafood.domain.service.VendaQueryService; 
+
+@Repository
+public class VendaQueryServiceImpl implements VendaQueryService {
+
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	@Override
+	public List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter filter) {
+
+		CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+		CriteriaQuery<VendaDiaria> query = builder.createQuery(VendaDiaria.class);
+		Root<Pedido> root = query.from(Pedido.class);
+
+		Expression<Date> expression = builder.function("date", Date.class, root.get("dataCriacao"));
+
+		CompoundSelection<VendaDiaria> selection = builder.construct(VendaDiaria.class, expression, builder.count(root.get("id")) , builder.sum(root.get("valorTotal")) );
+		query.select(selection);
+		query.groupBy(expression);
+		
+		
+		return entityManager.createQuery(query).getResultList();
+	}
+
+}
