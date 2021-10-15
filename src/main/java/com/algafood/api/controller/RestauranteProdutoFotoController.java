cd89a88cbf6e9ba142042b5e08ce4ded9867ef92
bruno <enzo_jpn@hotmail.com>
2021-10-14 22:56:23 -0300

    validacao para Fotoinput

--- src/main/java/com/algafood/api/controller/RestauranteProdutoFotoController.java
+++ src/main/java/com/algafood/api/controller/RestauranteProdutoFotoController.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.util.UUID;
 
+import javax.validation.Valid;
+
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -17,7 +19,7 @@ import com.algafood.api.model.input.FotoProdutoInput;
 public class RestauranteProdutoFotoController {
 
 	@PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-	public void atualizarFoto(@PathVariable Long restauranteId, @PathVariable Long produtoId, FotoProdutoInput fotoProdutoInput) {
+	public void atualizarFoto(@PathVariable Long restauranteId, @PathVariable Long produtoId, @Valid FotoProdutoInput fotoProdutoInput) {
 		
 		String nomeArquivo = UUID.randomUUID().toString() + "_" + fotoProdutoInput.getArquivo().getOriginalFilename();
 		
--- src/main/java/com/algafood/api/model/input/FotoProdutoInput.java
+++ src/main/java/com/algafood/api/model/input/FotoProdutoInput.java
@@ -1,6 +1,13 @@
 package com.algafood.api.model.input;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.http.MediaType;
 import org.springframework.web.multipart.MultipartFile;
+
+import com.algafood.core.validation.FileContentType;
+import com.algafood.core.validation.FileSize;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -9,8 +16,12 @@ import lombok.Setter;
 @Setter
 public class FotoProdutoInput {
 
-	
+	@NotNull
+	@FileSize(max = "999KB")
+	@FileContentType(allowed = {MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE})
 	private MultipartFile arquivo;
+
+	@NotBlank
 	private String descricao;
-	
+
 }
