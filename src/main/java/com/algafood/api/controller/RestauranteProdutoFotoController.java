67f17ce0f7d25471737f2d8ab8524c07f8216d4e
bruno <enzo_jpn@hotmail.com>
2021-10-23 22:25:03 -0300

    adicionando validação para mediaType

--- src/main/java/com/algafood/api/controller/RestauranteProdutoFotoController.java
+++ src/main/java/com/algafood/api/controller/RestauranteProdutoFotoController.java
@@ -2,6 +2,7 @@ package com.algafood.api.controller;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 import javax.validation.Valid;
 
@@ -9,9 +10,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -49,11 +52,25 @@ public class RestauranteProdutoFotoController {
 
 	@GetMapping(produces = MediaType.IMAGE_JPEG_VALUE)
 	public ResponseEntity<InputStreamResource> servirFoto(@PathVariable Long restauranteId,
-			@PathVariable Long produtoId) {
+			@PathVariable Long produtoId, @RequestHeader(name = "accept") String acceptHeader) throws HttpMediaTypeNotAcceptableException {
 		FotoProduto fotoProduto = catalogoProdutoService.buscarOuFalhar(restauranteId, produtoId);
+		
+		
+		MediaType mediaTypeFoto =MediaType.parseMediaType(fotoProduto.getContentType());
+		List<MediaType> mediasAceitas =  MediaType.parseMediaTypes(acceptHeader);
+		verificarCompatibilidadeMediaType(mediaTypeFoto, mediasAceitas);
+		
 		InputStream inputStream = fotoStorageService.recuperar(fotoProduto.getNomeArquivo());
 
 		return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(new InputStreamResource(inputStream));
+	}
+
+	private void verificarCompatibilidadeMediaType(MediaType mediaTypeFoto, List<MediaType> mediasAceitas) throws HttpMediaTypeNotAcceptableException {
+
+		boolean compativel = mediasAceitas.stream().anyMatch(mediasAceita -> mediasAceita.isCompatibleWith(mediaTypeFoto));
+		if(!compativel) {
+			throw new HttpMediaTypeNotAcceptableException(mediasAceitas);
+		}
 	}
 
 	@PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
@@ -61,9 +78,7 @@ public class RestauranteProdutoFotoController {
 			@Valid FotoProdutoInput fotoProdutoInput) throws IOException {
 
 		Produto produto = cadastroProdutoService.buscarOuFalhar(produtoId, restauranteId);
-
 		MultipartFile arquivo = fotoProdutoInput.getArquivo();
-
 		FotoProduto foto = new FotoProduto();
 
 		foto.setProduto(produto);
